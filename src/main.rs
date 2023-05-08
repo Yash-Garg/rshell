a744403b5a76dbe175d51cc04b0cb5ec786418b9
Yash-Garg <yash.garg@outlook.in>
2023-05-08 20:15:23 +0530

    add user input / arguments parsing and execution

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,71 @@
-use std::ffi::CString;
+use std::{
+    ffi::CString,
+    io::{stdin, stdout, Write},
+};
 
-use nix::unistd::execvp;
-
-fn main() {
-    let ls_cmd = Command {
-        program: "ls".to_string(),
-        args: vec!["-l".to_string(), "-a".to_string()],
-    };
-
-    exec_external_command(ls_cmd);
-}
+use nix::{
+    errno::Errno,
+    libc,
+    sys::wait::{waitpid, WaitPidFlag},
+    unistd::{execvp, fork, write, ForkResult},
+};
 
 #[derive(Debug, Clone)]
 struct Command {
     program: String,
-    args: Vec<String>,
+    args: Vec<CString>,
 }
 
-fn exec_external_command(cmd: Command) {
-    let args = cmd
-        .args
-        .iter()
-        .map(|arg| CString::new(arg.as_str()).unwrap())
-        .collect::<Vec<_>>();
+impl Command {
+    fn get_input() -> Self {
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    match execvp(&CString::new(cmd.program).unwrap(), &args) {
-        Ok(_) => {}
-        Err(err) => eprintln!("{}", err),
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap_or_default();
+
+        let args = parts
+            .map(|arg| CString::new(arg).unwrap())
+            .collect::<Vec<_>>();
+
+        Self {
+            program: command.to_string(),
+            args,
+        }
     }
+}
+
+fn main() {
+    loop {
+        print!("rshell> ");
+        stdout().flush().unwrap();
+
+        let cmd = Command::get_input();
+
+        match unsafe { fork() } {
+            Ok(ForkResult::Parent { child, .. }) => {
+                if child.as_raw() != 0 {
+                    waitpid(child, Some(WaitPidFlag::WUNTRACED)).unwrap();
+                }
+            }
+
+            Ok(ForkResult::Child) => {
+                if cmd.program.is_empty() {
+                    continue;
+                }
+
+                let result = execvp(&CString::new(cmd.program).unwrap(), &cmd.args);
+                match result {
+                    Ok(_) => {}
+                    Err(e) => eprint(e),
+                }
+            }
+
+            Err(e) => eprint(e),
+        }
+    }
+}
+
+fn eprint(error: Errno) {
+    write(libc::STDOUT_FILENO, format!("{}\n", error).as_bytes()).unwrap();
 }
